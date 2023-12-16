8dba8e8ea4ec2965a532d4fa41c1aff9ad947f32
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2023-12-16 21:37:36 +0700

    Added cancelAlarm function to each Alarm 1 and Alarm 2
    Since our alarm right now is hardcoded, i can just hardcode each alarm to :
    1. setAlarm1() and cancelAlarm1()
    2. setAlarm2() and cancelAlarm2()
    
    So, each setAlarm has their cancelAlarm.
    To invoke cancelAlarm to each Alarm, before that i need to declare pendingIntent outside of the onCreate so it can be accessed globally within the class.
    
    And, to invoke cancelAlarm, i only need to declare .cancel() into the pendingIntent and wrap it inside cancelAlarm function.

--- app/src/main/java/com/putragandad/alarmmanagerexercise/MainActivity.kt
+++ app/src/main/java/com/putragandad/alarmmanagerexercise/MainActivity.kt
@@ -21,7 +21,9 @@ import java.util.UUID
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
     private lateinit var alarmManager: AlarmManager
-    private lateinit var pendingIntent: PendingIntent
+
+    private lateinit var alarmManagerIntent1 : PendingIntent
+    private lateinit var alarmManagerIntent2 : PendingIntent
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,26 +40,23 @@ class MainActivity : AppCompatActivity() {
             setAlarm2("This is Alarm 2")
         }
 
-        binding.btnCancelAlarm.setOnClickListener {
-            cancelAlarm()
-        }
+
     }
 
-    private fun cancelAlarm() {
-        alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
-        val intent = Intent(this, AlarmReceiver::class.java)
+    private fun cancelAlarm1() {
+        alarmManagerIntent1.cancel()
+        Toast.makeText(this, "Alarm 1 now has been disabled", Toast.LENGTH_LONG).show()
+    }
 
-        pendingIntent = PendingIntent.getBroadcast(this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT)
-
-        alarmManager.cancel(pendingIntent)
-
-        Toast.makeText(this, "Alarm cancelled", Toast.LENGTH_LONG).show()
+    private fun cancelAlarm2() {
+        alarmManagerIntent2.cancel()
+        Toast.makeText(this, "Alarm 2 now has been disabled", Toast.LENGTH_LONG).show()
     }
 
     private fun setAlarm1(alarmNotificationText: String) {
         alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
 
-        val alarmManagerIntent = getAlarmIntent(alarmNotificationText)
+        alarmManagerIntent1 = getAlarmIntent(alarmNotificationText)
 
         val calendar: Calendar = Calendar.getInstance().apply {
             timeInMillis = System.currentTimeMillis()
@@ -69,7 +68,7 @@ class MainActivity : AppCompatActivity() {
             AlarmManager.RTC_WAKEUP,
             calendar.timeInMillis,
             AlarmManager.INTERVAL_DAY,
-            alarmManagerIntent
+            alarmManagerIntent1
         )
 
         Toast.makeText(this, "Alarm set successfullly", Toast.LENGTH_LONG).show()
@@ -78,7 +77,7 @@ class MainActivity : AppCompatActivity() {
     private fun setAlarm2(alarmNotificationText: String) {
         alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
 
-        val alarmManagerIntent = getAlarmIntent(alarmNotificationText)
+        alarmManagerIntent2 = getAlarmIntent(alarmNotificationText)
 
         val calendar: Calendar = Calendar.getInstance().apply {
             timeInMillis = System.currentTimeMillis()
@@ -90,7 +89,7 @@ class MainActivity : AppCompatActivity() {
             AlarmManager.RTC_WAKEUP,
             calendar.timeInMillis,
             AlarmManager.INTERVAL_DAY,
-            alarmManagerIntent
+            alarmManagerIntent2
         )
 
         Toast.makeText(this, "Alarm set successfullly", Toast.LENGTH_LONG).show()
